0f4c2f0feeabe9f9aaf47c606fcd0fb3abb5196b
tls0000 <jun22078@gmail.com>
2024-03-20 15:05:15 +0900

    practice_01

---
+++ src/main/java/practice_01.java
@@ -0,0 +1,20 @@
+public class practice_01 {
+    public static void main(String[] args) {
+        for (int i = 1; i < 5; i++) {
+            for (int a = 0; a < i; a++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        for (int i = 0; i < 5; i++) {
+            for (int a = 4; a > i; a--) {
+                System.out.print('*');
+            }
+            System.out.println();
+        }
+    }
+}
+
+
+
