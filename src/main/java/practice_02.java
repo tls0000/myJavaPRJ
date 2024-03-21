49bc38e16af1965b32da74484d1732c81f1825f0
tls0000 <jun22078@gmail.com>
2024-03-21 12:46:47 +0900

    practice_02

--- src/main/java/practice_02.java
+++ src/main/java/practice_02.java
@@ -1,21 +1,21 @@
 public class practice_02 {
     public static void main(String[] args) {
-        for(int i=0; i<4; i++){
-            for(int j=0; j<4; j++){
-                if(i>=j){
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+                if (i >= j) {
                     System.out.print("*");
-                }else{
+                } else {
                     System.out.print("");
                 }
 
             }
             System.out.println("");
         }
-        for(int i=3; i>0; i--){
-            for(int j=3; j>0; j--){
-                if(i>=j){
+        for (int i = 3; i > 0; i--) {
+            for (int j = 3; j > 0; j--) {
+                if (i >= j) {
                     System.out.print("*");
-                }else{
+                } else {
                     System.out.print("");
                 }
 
