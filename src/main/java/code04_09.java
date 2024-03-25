13b1e1ac1bef0984408841e0563b6fd297c70bb6
tls0000 <jun22078@gmail.com>
2024-03-25 13:35:21 +0900

    code04_09

---
+++ src/main/java/code04_09.java
@@ -0,0 +1,9 @@
+public class code04_09 {
+    public static void main(String[] args) {
+        System.out.println("\n줄바꿈\n연습 ");
+        System.out.println("\t탭키\t연습");
+        System.out.println("어떤 글자를 \"강조\"하는 효과");
+        System.out.println("어떤 글자를 \'강조\'하는 효과2");
+        System.out.println("\\\\ 백슬래쉬 2개 출력");
+    }
+}
