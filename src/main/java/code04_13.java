0b53ab50bc7d80439428052e90fe30754a41a667
tls0000 <jun22078@gmail.com>
2024-03-25 14:18:47 +0900

    code04_13

--- src/main/java/code04_13.java
+++ src/main/java/code04_13.java
@@ -1,11 +1,20 @@
 public class code04_13 {
     public static void main(String[] args) {
         String str ="   한글 ABCD efgh ";
-        String cutStr ="";
+        String upper ="";
+        String lower = "";
+        String cutStr = "";
+
+        upper = str.toUpperCase();
+        lower = str.toUpperCase();
+        cutStr = str.trim();
 
         cutStr = str.trim();
 
+
         System.out.println("기존 문자열 ==>[" + str + "]");
         System.out.println("공백 제거 ==> [" + cutStr + "]");
+        System.out.println("대문자 ==>[" + upper + "]");
+        System.out.println("소문자 ==>[" + lower + "]");
     }
 }
