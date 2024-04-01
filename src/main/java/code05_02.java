5ed05c789473e989be99dda4d5c723e308294faa
tls0000 <jun22078@gmail.com>
2024-04-01 14:21:07 +0900

    code05_02

---
+++ src/main/java/code05_02.java
@@ -0,0 +1,11 @@
+public class code05_02 {
+    public static void main(String[] args) {
+        int num = 99;
+
+        if (num < 100) {
+            System.out.print("100보다");
+            System.out.print("작습니다.");
+        }
+        System.out.println("난 조건절과 상관없이 무조건 실향된다.");
+    }
+}
