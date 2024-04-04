e4e5ef5fcb0df3cacbe322413690565388e55f7f
tls0000 <jun22078@gmail.com>
2024-04-04 10:04:54 +0900

    practice_01

--- src/main/java/practice_01.java
+++ src/main/java/practice_01.java
@@ -1,20 +1,31 @@
 public class practice_01 {
     public static void main(String[] args) {
-        for (int i = 1; i < 5; i++) {
-            for (int a = 0; a < i; a++) {
-                System.out.print("*");
-            }
-            System.out.println();
-        }
+        for (int a = 0; a < 5; a++) {
+            for (int b = 0; b < 5; b++) {
+                if (a >= b) {
+                    System.out.print("*");
+                } else {
+                    System.out.print("");
+                }
 
-        for (int i = 0; i < 4; i++) {
-            for (int a = 3; a > i; a--) {
-                System.out.print('*');
             }
-            System.out.println();
+            System.out.println("");
+        }
+        for (int c = 5; c < 0; c++) {
+            for (int d = 5; d > 0; d--) {
+                if (c >= d) {
+                    System.out.print("*");
+                } else {
+                    System.out.print("");
+                }
+
+            }
+                System.out.println("");
+            }
         }
     }
-}
+
+
 
 
 
