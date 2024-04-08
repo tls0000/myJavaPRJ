ec3df29c316b5b801d525a39ef0da7f4b5f920ca
tls0000 <jun22078@gmail.com>
2024-04-08 10:39:33 +0900

    practice_10

---
+++ src/main/java/practice_10.java
@@ -0,0 +1,11 @@
+public class practice_10 {
+    public static void main(String[] args) {
+        String msg = "Spring is beginning";
+        int a = 3;
+        String b = "3";
+
+        System.out.println(msg);
+        System.out.println(a + 10);
+        System.out.println(b + "10");
+    }
+}
