e290c8f482bff9fcbcbaeb168b396c267f87d756
tls0000 <jun22078@gmail.com>
2024-04-17 18:34:23 +0900

    code06_09

--- src/main/java/code06_09.java
+++ src/main/java/code06_09.java
@@ -1,10 +1,17 @@
 public class code06_09 {
     public static void main(String[] args) {
 
-        for (int i=0 ; i<3 ; i++){
-            for (int k=0 ; k<2 ; k++){
-                System.out.println("난생처음은 쉽습니다.^^"+"(i:"+i+",k:"+k+")");
+        for (int i = 2; i <= 9; i++) {
+
+            for (int j = 1; j <= 9; j++) {
+                System.out.println(i + " x " + j + " = " + i * j);
+            }
+
+            //for (int i = 1; i <= 9; i++) {
+                // for (int k=0 ; k<2 ; k++){
+                // System.out.println("난생처음은 쉽습니다.^^"+"(i:"+i+",k:"+k+")");
+                //System.out.println("3 x " + i + " = " + 3 * i);
             }
         }
     }
-}
+
