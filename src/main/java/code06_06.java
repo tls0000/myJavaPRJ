eee7ef1620a46d1acdc0ada497ef489b9fd832dc
tls0000 <jun22078@gmail.com>
2024-04-18 11:13:32 +0900

    code06_06

---
+++ src/main/java/code06_06.java
@@ -0,0 +1,9 @@
+public class code06_06 {
+    public static void main(String[] args) {
+        int hap = 0;
+        for (int i = 1 ; i<=10 ; i++) {
+            hap = hap + i;
+        }
+        System.out.println("1부터 10까지의 합계 : " + hap);
+    }
+}
