406a3f2c8301bd2259680eb74cedd85b7e218e67
tls0000 <jun22078@gmail.com>
2024-04-18 11:11:47 +0900

    code06_01

---
+++ src/main/java/code06_01.java
@@ -0,0 +1,7 @@
+public class code06_01 {
+    public static void main(String[] args) {
+        System.out.println("난생처음 자바는 재미있습니다. ^^");
+        System.out.println("난생처음 자바는 재미있습니다. ^^");
+        System.out.println("난생처음 자바는 재미있습니다. ^^");
+    }
+}
