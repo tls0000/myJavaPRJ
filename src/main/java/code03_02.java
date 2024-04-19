68bb36c3504dfb0301cf27187497a9acdfa0f191
tls0000 <jun22078@gmail.com>
2024-04-19 15:12:06 +0900

    code03_02

---
+++ src/main/java/code03_02.java
@@ -0,0 +1,17 @@
+public class code03_02 {
+    public static void main(String[] args) {
+        int a,b,c;
+
+        a = 5;
+        b = 3;
+        c = a+b;
+
+        System.out.println(c);
+        c = a-b;
+        System.out.println(c);
+        c = a*b;
+        System.out.println(c);
+        c = a/b;
+        System.out.println(c);
+    }
+}
