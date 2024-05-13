9d66f63d12bcf1cc2703229749b36f0c66bd3bb2
tls0000 <jun22078@gmail.com>
2024-05-13 17:34:45 +0900

    exam04

---
+++ src/main/java/exam04.java
@@ -0,0 +1,13 @@
+class Car {
+    private int speed;
+    private String color;
+
+    Car(int speed) {
+        this.speed = speed;
+    }
+}
+public class exam04 {
+    public static void main(String[] args) {
+        Car car1=new Car(100);
+    }
+}
