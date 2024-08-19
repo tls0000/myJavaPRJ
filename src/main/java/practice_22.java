dfdba24314119b7aac84a6f2dc878ad1f7490778
tls0000 <jun22078@gmail.com>
2024-08-19 16:07:24 +0900

    practice_22

---
+++ src/main/java/practice_22.java
@@ -0,0 +1,12 @@
+public class practice_22 {
+        public static void main(String[] args) {
+            int a = 3; // 예시 값
+            int c = 5; // 예시 값
+
+            // b^2 계산
+            int b_square = (c * c) - (a * a);
+
+            // b^2 출력
+            System.out.println("b^2 = " + b_square);
+        }
+    }
